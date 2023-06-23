42aa0582be58f99c03bab17d832aa74d2d3e194a
YudiRamadhan <99422747+YudiHaxor@users.noreply.github.com>
2023-06-24 05:40:12 +0700

    Update yuk.php

--- yuk.php
+++ yuk.php
@@ -1,1 +1,12 @@
-Yudi
+<div style="display:none;">
+<a href="http://presensi.pacitankab.go.id/">togel</a>
+<a href="http://presensi.pacitankab.go.id/">bandar togel</a>
+<a href="http://presensi.pacitankab.go.id/protected/storage/data-macau/">data macau</a>
+<a href="http://presensi.pacitankab.go.id/protected/storage/data-macau/">kolam makau</a>
+<a href="http://presensi.pacitankab.go.id/protected/storage/situs-togel/">togel</a>
+<a href="http://presensi.pacitankab.go.id/protected/storage/situs-togel/">situs togel</a>
+<a href="http://presensi.pacitankab.go.id/protected/storage/slot88/">http://presensi.pacitankab.go.id/protected/storage/slot88/</a>
+<a href="http://presensi.pacitankab.go.id/protected/storage/slot88/">slot gacor</a>
+<a href="http://presensi.pacitankab.go.id/protected/storage/slot-online/">http://presensi.pacitankab.go.id/protected/storage/slot-online/</a >
+<a href="http://presensi.pacitankab.go.id/protected/storage/slot-online/">slot gacor</a>
+</div>
